64078aa2925f08f2c4f82dc330b85f4f57f5f1cb
Yonathan Castillo <leothan522@gmail.com>
2026-03-14 13:25:46 -0400

    Export Excel Estudiantes

--- app/Filament/Resources/Estudiantes/Tables/EstudiantesTable.php
+++ app/Filament/Resources/Estudiantes/Tables/EstudiantesTable.php
@@ -2,6 +2,8 @@
 
 namespace App\Filament\Resources\Estudiantes\Tables;
 
+use App\Models\Estudiante;
+use Carbon\Carbon;
 use Filament\Actions\Action;
 use Filament\Actions\ActionGroup;
 use Filament\Actions\BulkActionGroup;
@@ -12,9 +14,14 @@ use Filament\Actions\RestoreBulkAction;
 use Filament\Actions\ViewAction;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
 use Illuminate\Support\Str;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+use pxlrbt\FilamentExcel\Actions\ExportBulkAction;
+use pxlrbt\FilamentExcel\Columns\Column;
+use pxlrbt\FilamentExcel\Exports\ExcelExport;
 
 class EstudiantesTable
 {
@@ -43,6 +50,7 @@ class EstudiantesTable
                     ->visibleFrom('md'),
                 TextColumn::make('cedula')
                     ->label('C.I. / Cédula E.')
+                    ->placeholder('-')
                     ->numeric()
                     ->alignCenter()
                     ->searchable()
@@ -62,6 +70,21 @@ class EstudiantesTable
                     ->visibleFrom('lg'),
             ])
             ->filters([
+                SelectFilter::make('colegio')
+                    ->relationship('colegio', 'nombre')
+                    ->preload()
+                    ->searchable(['codigo', 'nombre'])
+                    ->visible(isAdmin()),
+
+                SelectFilter::make('deportes')
+                    ->relationship('deportes', 'nombre')
+                    ->preload()
+                    ->searchable(),
+                SelectFilter::make('sexo')
+                    ->options([
+                        'femenino' => 'Femenino',
+                        'masculino' => 'Masculino',
+                    ]),
                 TrashedFilter::make(),
             ])
             ->recordActions([
@@ -76,9 +99,69 @@ class EstudiantesTable
                     ForceDeleteBulkAction::make(),
                     RestoreBulkAction::make(),
                 ]),
+                self::actionExportExcel(),
                 Action::make('actualizar')
                     ->icon(Heroicon::ArrowPath)
                     ->iconButton(),
             ]);
+    }
+
+    protected static function actionExportExcel()
+    {
+        return ExportBulkAction::make()->exports([
+            ExcelExport::make()
+                ->withFilename('estudiantes-export')
+                ->withColumns([
+                    Column::make('colegio.codigo')
+                        ->heading('COD Colegio')
+                        ->formatStateUsing(fn (string $state): string => Str::upper($state))
+                        ->format(NumberFormat::FORMAT_TEXT),
+                    Column::make('nombres')
+                        ->formatStateUsing(fn (string $state): string => Str::upper($state)),
+                    Column::make('apellidos')
+                        ->formatStateUsing(fn (string $state): string => Str::upper($state)),
+                    Column::make('cedula')
+                        ->heading('C.I./Cédula Estudiantil')
+                        ->format(NumberFormat::FORMAT_NUMBER),
+                    Column::make('fecha_nacimiento')
+                        ->heading('Nacimiento')
+                        ->formatStateUsing(fn ($state) => Carbon::parse($state)->format('d/m/Y'))
+                        ->format(NumberFormat::FORMAT_DATE_DDMMYYYY),
+                    Column::make('edad')
+                        ->format(NumberFormat::FORMAT_NUMBER),
+                    Column::make('sexo'),
+                    Column::make('deportes')
+                        ->formatStateUsing(fn ($state) =>
+                            // $state será una colección de modelos Deporte
+                        $state ? $state->pluck('nombre')->implode(', ') : 'Ninguno'
+                        ),
+                    Column::make('representante.nombre')
+                        ->heading('Nombre Representante')
+                        ->formatStateUsing(fn (string $state): string => Str::upper($state)),
+                    Column::make('representante.cedula')
+                        ->heading('Cédula Representante')
+                        ->format(NumberFormat::FORMAT_NUMBER),
+                    Column::make('representante.telefono')
+                        ->heading('Teléfono Representante')
+                        ->formatStateUsing(fn (string $state): string => Str::upper($state))
+                        ->format(NumberFormat::FORMAT_TEXT),
+                    Column::make('representante.telefono_2')
+                        ->heading('Teléfono Secundario')
+                        ->formatStateUsing(fn (string $state): string => Str::upper($state))
+                        ->format(NumberFormat::FORMAT_TEXT),
+                    Column::make('direccion_representante')
+                        ->heading('¿Vive con representante?')
+                        ->formatStateUsing(fn (bool $state): string => $state ? 'SI' : 'NO'),
+                    Column::make('states_id')
+                        ->heading('Estado')
+                        ->getStateUsing(fn (Estudiante $record) => $record->direccion_representante ? $record->representante->estado->name : $record->estado->name),
+                    Column::make('municipalities_id')
+                        ->heading('Municipio')
+                        ->getStateUsing(fn (Estudiante $record) => $record->direccion_representante ? $record->representante->municipio->name : $record->municipio->name),
+                    Column::make('parishes_id')
+                        ->heading('Parroquia')
+                        ->getStateUsing(fn (Estudiante $record) => $record->direccion_representante ? $record->representante->parroquia->name : $record->parroquia->name),
+                ]),
+        ]);
     }
 }

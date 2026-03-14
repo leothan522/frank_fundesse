fa37fcbb84006884054f6a261c63fdb79217c6b2
Yonathan Castillo <leothan522@gmail.com>
2026-03-14 14:12:50 -0400

    Estudiantes Widget

---
+++ app/Filament/Widgets/EstudiantesStats.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Estudiante;
+use Filament\Support\Enums\IconPosition;
+use Filament\Support\Icons\Heroicon;
+use Filament\Widgets\StatsOverviewWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class EstudiantesStats extends StatsOverviewWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('Total Estudiantes', $this->getEstudiantes())
+                ->description('Registrados en el sistema')
+                ->descriptionIcon(Heroicon::OutlinedUserGroup, IconPosition::Before)
+                ->chart([7, 3, 4, 5, 6, 3, 5, 10]) // Opcional: una línea de tendencia
+                ->color('primary'),
+
+            Stat::make('Masculinos', $this->getMasculinos())
+                ->description('Estudiantes varones')
+                ->descriptionIcon('heroicon-m-user', IconPosition::Before)
+                ->color('info'),
+
+            Stat::make('Femeninas', $this->getFemeninas())
+                ->description('Estudiantes hembras')
+                ->descriptionIcon('heroicon-m-user', IconPosition::Before)
+                ->color('success'),
+        ];
+    }
+
+    protected function getEstudiantes(): int
+    {
+        $query = Estudiante::query();
+        if (! isAdmin()) {
+            $query->where('colegios_id', auth()->user()->colegios_id);
+        }
+
+        return $query->count();
+    }
+
+    protected function getMasculinos(): int
+    {
+        $query = Estudiante::query();
+        $query->where('sexo', 'masculino');
+        if (! isAdmin()) {
+            $query->where('colegios_id', auth()->user()->colegios_id);
+        }
+
+        return $query->count();
+    }
+
+    protected function getFemeninas(): int
+    {
+        $query = Estudiante::query();
+        $query->where('sexo', 'femenino');
+        if (! isAdmin()) {
+            $query->where('colegios_id', auth()->user()->colegios_id);
+        }
+
+        return $query->count();
+    }
+}
